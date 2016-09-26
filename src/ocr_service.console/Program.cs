706d23d627b6d10d7744630a9a4aafb70e6642e8
schwamster <bastian.toepfer@bodoconsult.de>
2016-09-26 22:44:24 +0200

    first commit

---
+++ src/ocr_service.console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ocr_service.console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subscriptionKey = "no key";
+            var path = @"no file";
+            if (args.Length == 4)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "-s")
+                    {
+                        subscriptionKey = args[i + 1];
+                    }
+
+                    if (args[i] == "-p")
+                    {
+                        path = args[i + 1];
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("usage: ocr_service -s <subscriptionkey> -p <pathToImage>");
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine($"uploading Image '{path}' with you subsciption {subscriptionKey.Substring(0, 5)}...");
+
+            var service = new OcrService();
+
+            var result = service.ExtractTextFromFile(subscriptionKey, path);
+
+            Console.WriteLine(result.Result);
+        }
+    }
+}
---
+++ src/ocr_service/OcrService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace ocr_service
+{
+    public class OcrService
+    {
+        public const string baseAdress = "https://api.projectoxford.ai/vision/v1.0/ocr?language=en";
+        public const string subscriptionHeaderName = "ocp-apim-subscription-key";
+
+        public async System.Threading.Tasks.Task<string> ExtractTextFromFile(string subscriptionKey, string path)
+        {
+            using (var client = new HttpClient())
+            {
+                //client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "multipart-form");
+                client.DefaultRequestHeaders.TryAddWithoutValidation(subscriptionHeaderName, subscriptionKey);
+
+                using (var content = new MultipartFormDataContent())
+                {
+                    content.Add(new StreamContent(new FileStream(path, FileMode.Open)), "file", "upload.png");
+
+                    using (var message = await client.PostAsync(baseAdress, content))
+                    {
+                        var input = await message.Content.ReadAsStringAsync();
+
+                        return input;
+                    }
+                }
+            }
+        }
+
+        public OcrResult ParseResult(string json)
+        {
+            return new OcrResult();
+        }
+
+
+    }
+
+    public class OcrResult
+    {
+
+    }
+}
+
