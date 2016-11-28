58e93f93143cac2c13b0e9a7fe59155293a6dc9e
schwamster <bastian.toepfer@gmail.com>
2016-11-28 21:45:26 +0100

    rmrf position changed ;)

--- build-container.sh
+++ build-container.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 set -e
 dotnet restore
+rm -rf $(pwd)/testresults
 dotnet test test/ocr_service.Tests/project.json -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/publish/web
-rm -rf $(pwd)/testresults
 dotnet publish src/ocr_service.webapi/project.json -c release -o $(pwd)/publish/web
