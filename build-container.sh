bc8a675270bcd08c73e940d4926bbf2d0048eb00
schwamster <bastian.toepfer@gmail.com>
2016-11-28 21:20:55 +0100

    fixed build

--- build-container.sh
+++ build-container.sh
@@ -4,4 +4,4 @@ dotnet restore
 dotnet test test/ocr_service.Tests/project.json -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/publish/web
 rm -rf $(pwd)/testresults
-dotnet publish src/text_worker/project.json -c release -o $(pwd)/publish/web
+dotnet publish src/ocr_service.webapi/project.json -c release -o $(pwd)/publish/web
